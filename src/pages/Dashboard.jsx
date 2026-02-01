1622507e62958647988adf4a763ed3c57d70c060
0khushiwalia <walia.khushi@icloud.com>
2026-02-01 23:50:46 +0530

    Commit done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -106,7 +106,7 @@ export default function Dashboard() {
     }
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/predict`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ cycles }),
